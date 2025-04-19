bb8bf51c2122f101b9d70cfff547f6b407550ace
Matin Karbasioun <M.karbasion@irfarabi.com>
2025-04-19 15:25:13 +0330

    [feature] add priority queue class

---
+++ python/data_structures/priority_queue/priority_queue.py
@@ -0,0 +1,27 @@
+"""
+c++ --> priority_queue
+python --> heapq
+java --> PriorityQueue
+
+
+Real-world use case
+
+1- scheduling jobs
+- insert new job quickly
+- find next job quickly
+
+
+insert element with priority p
+extracts an element with maximum priority
+
+"""
+
+
+
+class PriorityQueue:
+    def insert(self, element):
+        pass
+
+    def extract_max(self):
+        pass
+
